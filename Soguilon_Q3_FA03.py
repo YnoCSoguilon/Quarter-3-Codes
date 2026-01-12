ead186d800d2cb21bc7a0d297ffe2becb698eae1
Yno C Soguilon <yno.soguilon@smc.pshs.edu.ph>
2026-01-12 22:51:45 +0800

    Create Soguilon_Q3_FA03.py

---
+++ Soguilon_Q3_FA03.py
@@ -0,0 +1,14 @@
+#my arrays
+groceries = ["apples", "biscuits", "candy", "dairy", "eggs", "flour", "garlic", "isda"]
+
+houses = [
+  [0], [A], [B], [C], [D],
+  [1], [A1], [B1], [C1], [D1],
+  [2], [A2], [B2], [C2], [D2],
+  [3], [A3], [B3], [C3], [D3],
+  [4], [A4], [B4], [C4], [D4],
+  [5], [A5], [B5], [C5], [D5]
+]
+
+#my codes
+print(houses[1][0])
