f672eabd668dd0fed51149718f627e3859317ebd
Yno C Soguilon <yno.soguilon@smc.pshs.edu.ph>
2026-01-12 22:39:54 +0800

    Add houses list to Soguilon_Q3_FA02.py

---
+++ Soguilon_Q3_FA02.py
@@ -0,0 +1,9 @@
+groceries = ["apples", "biscuits", "candy", "dairy", "eggs", "flour", "garlic", "isda"]
+
+houses = [
+  [A1], [B1], [C1], [D1],
+  [A2], [B2], [C2], [D2],
+  [A3], [B3], [C3], [D3],
+  [A4], [B4], [C4], [D4],
+  [A5], [B5], [C5], [D5]
+]
